93cfc31583d52730aafeca6b56f61d8ee6f74599
Luojm <luojunmei@212>
2021-02-22 00:27:13 +0800

    this is a test

---
+++ C4/pythoncode/Calculator.py
@@ -0,0 +1,15 @@
+#coding:utf-8
+import yaml
+
+
+class Calculator:
+    def add(self,a,b):
+        return a+b
+
+    def div(self,a,b):
+        return a/b
+
+    def getdatas(self):
+        with open("./data/result.yml") as f:
+            datas = yaml.safe_load(f)
+        return datas["add"]["add"]
